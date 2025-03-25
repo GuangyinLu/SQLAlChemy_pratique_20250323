ff7a1946bfe84ebda34dec6f94e04cfcca6044c8
Guangyin Lu <luguangyin.mtl@gmail.com>
2025-03-25 15:04:51 -0400

    axios

--- database.py
+++ database.py
@@ -4,8 +4,16 @@ from config import DB_CONFIG
 
 
 DATABASE_URL = f"mysql+pymysql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}/{DB_CONFIG['database']}"
+POOL_SIZE = 10           # Increase base pool size
+MAX_OVERFLOW = 20        # Increase overflow limit
+POOL_TIMEOUT = 30         # Timeout in seconds)
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, 
+                       pool_size = POOL_SIZE, 
+                       max_overflow = MAX_OVERFLOW,
+                       pool_timeout = POOL_TIMEOUT
+                       )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
